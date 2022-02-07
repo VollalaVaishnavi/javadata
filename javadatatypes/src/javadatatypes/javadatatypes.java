d52c883d7c3ab4ca6d6c6533938243e51f13ac34
vaishu vollala <vaishu vollala@LAPTOP-U09TFG76>
2022-02-07 10:29:41 +0530

    commit

---
+++ javadatatypes/src/javadatatypes/javadatatypes.java
@@ -0,0 +1,15 @@
+package javadatatypes;
+
+public class javadatatypes {
+
+	public static void main(String[] args) {
+		int a=10;
+		String b="hyderabad";
+		char c='A';
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+
+	}
+
+}
